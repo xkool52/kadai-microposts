82ea53937c086e70a37271a609a98c95640fbc8f
yuuta sumiyoshi <xkool52@gmail.com>
2020-06-22 10:21:28 +0000

    post

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -44,7 +44,7 @@ class MicropostsController extends Controller
     public function destroy($id)
     {
         // idの値で投稿を検索して取得
-        $micropost = Micropost::findOrFail($id);
+        $micropost = \App\Micropost::findOrFail($id);
 
         // 認証済みユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
         if (\Auth::id() === $micropost->user_id) {
